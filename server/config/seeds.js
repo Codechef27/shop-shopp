bdaa0a698c77f4bbdafd9ed022524ab95029bf2c
BradleyK <bradleyjkennett@gmail.com>
2022-09-08 23:47:08 -0400

    deploy didnt run seeds/ trying again

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -128,28 +128,6 @@ db.once('open', async () => {
 
   console.log('products seeded');
 
-  await User.deleteMany();
-
-  await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
-
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
-
-  console.log('users seeded');
 
   process.exit();
 });
